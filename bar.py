cb3699ecf40c9e726e510bd6ea22c9b7702738e8
Franklin <franklin@LT-32871.local>
2019-06-19 16:05:14 +0530

    Initial Commit

---
+++ bar.py
@@ -0,0 +1,20 @@
+print('Hello World!')
+
+print(
+"""
+What are the full forms of the 'vi',' vim', and 'gcc' commands in ...\
+https://www.quora.com/What-are-the-full-forms-of-the-vi-vim-and-gcc-commands-in...\
+Dec 6, 2014 - Vi(Visual Interactive) is a powerful text editor in Linux, so is vim(Vi IMproved).\
+What's the difference between the applications ...	4 answers	25 Nov 2015
+What is Vim? - Quora	7 answers	3 Feb 2014
+More results from www.quora.com
+"""
+)
+y='''What are the full forms of the \'vi\',\' vim\', and \'gcc\' commands in ...\
+https://www.quora.com/What-are-the-full-forms-of-the-vi-vim-and-gcc-commands-in...\
+Dec 6, 2014 - Vi(Visual Interactive) is
+More results from www.quora.com'''
+print(len(y))
+
+
+
---
+++ lemonup.py
@@ -0,0 +1,6 @@
+x=6-7
+#print('6 - 7 = '+str(x))
+print (7/2)
+word = 'python'
+print(word[4:]+word[:4])
+ 
